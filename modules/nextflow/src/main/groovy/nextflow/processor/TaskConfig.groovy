7c4c1355563bdd050f8f24b48f5da4af6a3200a7
Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>
2025-12-17 20:58:22 +0100

    Skip PublishDir
    Signed-off-by: Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>

--- modules/nextflow/src/main/groovy/nextflow/processor/TaskConfig.groovy
+++ modules/nextflow/src/main/groovy/nextflow/processor/TaskConfig.groovy
@@ -415,7 +415,7 @@ class TaskConfig extends LazyMap implements Cloneable {
 
     List<PublishDir> getPublishDir() {
         def dirs = get('publishDir')
-        if( !dirs ) {
+        if( !dirs || target.skipPublishDir ) {
             return Collections.emptyList()
         }
 
